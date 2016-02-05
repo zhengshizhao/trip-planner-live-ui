011a92e6153391e303a2b358a7daa5dd0edec12f
Ksenia Mikhailovskaya <ksenia0822@gmail.com>
2016-02-05 09:00:31 -0500

    Add and remove items done

--- public/js/index.js
+++ public/js/index.js
@@ -1,34 +1,50 @@
 $("#hotel_btn").on('click', function(){
-	var $currenHotel = $("#current_hotel").val();
-	//console.log($currenHotel);
-	var $newhotel = $('<span class="title">'+ $currenHotel+'</span>')
-	var $newbtn = $('<button id="remove_hotel" class="btn btn-xs btn-danger remove btn-circle">x</button>');
-    $newhotel.appendTo($("#hotel_list"));
-    $newbtn.appendTo($("#hotel_list"));
+	addItem($(this).prev(), $('#hotel_list'));
 })
 
+$("#restaurant_btn").on('click', function(){
+	addItem($(this).prev(), $('#restaurant_list'));
+})
+
+$("#activity_btn").on('click', function(){
+	addItem($(this).prev(), $('#activity_list'));
+})
+
+// $('.add').on('click', function() {
+// 	var list = $(this).parent().val()
+// 	list = list + "_list";
+// 	addItem($(this).prev(), $(list));
+// })
+
+function addItem(this_element, appendToEl) {
+	var $newElement = $('<span class="title">'+ this_element.val() +'</span>')
+	var $newbtn = $('<button class="btn btn-xs btn-danger remove btn-circle">x</button>');
+    $newElement.appendTo($(appendToEl));
+    $newbtn.appendTo($(appendToEl));
+    //save this item in that day in that 
+}
 
 function removeItem(this_element){
-	console.log($this);
-	$this.hide();
-	$this.prev().hide();
-
+	this_element.hide();
+	this_element.prev().hide();
 }
 
 $("#hotel_list").on('click', '.remove', function(){
-	var $this = $(this);
-	removeItem(this_element);
-
+	removeItem($(this));
 })
-// $("#remove_hotel").on('click', function(){
-// 	console.log($(this));
-// 	$(this).hide();
-// 	$(this).prev().hide();
-
-// })
 
 
+$("#restaurant_list").on('click', '.remove', function(){
+	removeItem($(this));
+})
 
+$("#activity_list").on('click', '.remove', function(){
+	removeItem($(this));
+})
+
+
+
+//method that will look at the state of things at the dom, analyze and update
 
 
 
